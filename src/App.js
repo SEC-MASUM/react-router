d4651a96749d77191797ec5d47fb35f5e6063d46
SEC-MASUM <masum.jess42@gmail.com>
2022-04-02 01:37:36 +0600

    added routes of components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Friends from "./components/Friends/Friends";
 import About from "./components/About/About";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
+      <h1>Welcome to my fancy Routing Website</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }

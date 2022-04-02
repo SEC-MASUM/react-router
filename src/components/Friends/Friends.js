87ca8647309a386115b7fc481dde907b6740d77d
SEC-MASUM <masum.jess42@gmail.com>
2022-04-02 08:17:42 +0600

    add friend components and loaded data from json placeholder

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,12 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Friend from "../Friend/Friend";
 
 const Friends = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://jsonplaceholder.typicode.com/users")
+      .then((data) => setFriends(data.data));
+  }, []);
   return (
     <div>
       <h2>How are you?</h2>
       <p>
-        <small>Parle kicu tk dhar dish...</small>
+        <small>Parle kicu tk dhar dish...:{friends.length}</small>
       </p>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend}></Friend>
+      ))}
     </div>
   );
 };

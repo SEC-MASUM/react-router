53c1d3361b2705f5c95e3c3c10ba956136ae0f2a
SEC-MASUM <masum.jess42@gmail.com>
2022-04-02 09:37:55 +0600

    added navigation link in header and styled it

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
+      <nav class="bg-gray-100 font-sans w-full m-0">
+        <div class="bg-white shadow">
+          <div class="container max-w-7xl mx-auto px-4">
+            <div class="flex items-center justify-between py-4">
+              <div class="sm:flex sm:items-center">
+                <Link
+                  to="/"
+                  class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/friends"
+                  class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4"
+                >
+                  Friends
+                </Link>
+                <Link
+                  to="/about"
+                  class="text-gray-800 text-sm font-semibold hover:text-purple-600"
+                >
+                  About
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
       <h1>Welcome to my fancy Routing Website!!!</h1>
     </div>
   );
